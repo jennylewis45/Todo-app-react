4ef40f792648409cf9e9b4a9c133d0a3777fa094
Amirkhan567 <amrkh5678@gmail.com>
2023-10-01 19:51:34 +0500

    todoapp first update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todo/>
     </div>
   );
 }
 
+
+
+function Todo({ text ,onSelect,id}) {
+
+return (
+<>
+<div className="todo_style">
+
+<i onClick={()=>{
+onSelect(id)
+}} className="fa fa-times" aria-hidden="true"></i>
+<li>{text}</li>
+</div>
+
+
+</>
+);
+}
+
 export default App;
+
+
+
