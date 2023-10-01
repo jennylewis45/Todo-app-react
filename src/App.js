d59c93fecf30e593eef625d0f862c64fa102ddb0
Javeriya Imran <121876323+jennylewis45@users.noreply.github.com>
2023-10-01 20:43:14 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,66 @@
+import React, { useState } from "react";
 
-import './App.css';
+import "./App.css";
+import Todo from "./Components/Todo";
 
 function App() {
+  const [inputList, setinputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const Additem = (e) => {
+    setinputList(e.target.value);
+  };
+
+  const Add = () => {
+    if (inputList.trim() !== "") {
+      setItems((olditems) => {
+        return [...olditems, inputList];
+      });
+      setinputList("");
+    }
+  };
+
+  const deleteitem = (id) => {
+    console.log("delete");
+    setItems((olditems) => {
+      return olditems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <Todo/>
-    </div>
+    <>
+    <h1>Todo App by React</h1>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input
+            type="text"
+            value={inputList}
+            placeholder="Add Items"
+            onChange={Additem}
+          />
+          <button onClick={Add}>+</button>
+          <ol>
+            {items.map((itemvalue, index) => {
+              return (
+               <Todo
+               text={itemvalue}
+               key={index}
+               id={index}
+               onSelect={deleteitem}
+               />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+      
+    </>
   );
 }
 
-
-
-function Todo({ text ,onSelect,id}) {
-
-return (
-<>
-<div className="todo_style">
-
-<i onClick={()=>{
-onSelect(id)
-}} className="fa fa-times" aria-hidden="true"></i>
-<li>{text}</li>
-</div>
-
-
-</>
-);
-}
-
 export default App;
-
-
-
